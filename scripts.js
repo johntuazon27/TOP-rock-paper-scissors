28b0b5789d297752d4b6b70142ed4a0cf77057cc
tachuan <54359149+johntuazon27@users.noreply.github.com>
2024-07-17 21:36:42 +0900

    Add humanScore and computerScore and initialize the value to 0

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
  const choices = ["rock", "paper", "scrissors"]
+ const humanScore = 0
+ const computerScore = 0
  
  function getComputerChoice () {
     const randomIndex = Math.floor(Math.random() * choices.length)
