b3a383facab6917d190a6d286e939ce47748fefb
tachuan <54359149+johntuazon27@users.noreply.github.com>
2024-07-18 20:58:19 +0900

    Add playRound function
    Check whether who wins and increase then point of the winner

--- scripts.js
+++ scripts.js
@@ -1,17 +1,40 @@
- const choices = ["rock", "paper", "scrissors"]
- const humanScore = 0
- const computerScore = 0
- 
- function getComputerChoice () {
+const choices = ["ROCK", "PAPER", "SCISSORS"]
+let humanScore = 0
+let computerScore = 0
+
+
+function getComputerChoice () {
     const randomIndex = Math.floor(Math.random() * choices.length)
+    console.log(choices[randomIndex])
     return choices[randomIndex]
- }
+}
 
- console.log(getComputerChoice())
 
- function getHumanChoice() {
+
+function getHumanChoice() {
     humanChoice = prompt()
-    return humanChoice
- }
+    console.log(humanChoice.toUpperCase())
+    return humanChoice.toUpperCase()
+}
 
-console.log(getHumanChoice())
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice ==  computerChoice) {
+        console.log("RESULT: DRAW!")
+    } else if (humanChoice == "ROCK" && computerChoice == "SCISSORS") {
+        humanScore++
+        console.log("YOU WIN!") 
+    }else if (humanChoice == "PAPER" && computerChoice == "ROCK") {
+        humanScore++
+        console.log("YOU WIN!") 
+    }else if (humanChoice == "SCISSORS" && computerChoice == "PAPER") {
+        humanScore++
+        console.log("YOU WIN!") 
+    } else {
+        computerScore++
+        console.log("YOU LOSE!")
+    }
+    console.log(`CURRENT SCORE\nYOU:${humanScore}\nCOMPUTER:${computerScore}`)
+}
+
+playRound(getHumanChoice(), getComputerChoice())
